4f8ab50a3e0ff95c27f425967c47263b315bb088
Maik Glatki <maik.glatki@gmail.com>
2016-06-06 14:20:05 +0000

    Basic puppet module files.

---
+++ manifests/init.pp
@@ -0,0 +1,28 @@
+# == Class: oraclejava
+#
+# TBD
+#
+# === Parameters
+#
+# TBD
+#
+# === Variables
+#
+# TBD
+#
+# === Examples
+#
+# TBD
+#
+# === Authors
+#
+# Maik Glatki <maik.glatki@gmail.com>
+#
+# === Copyright
+#
+# Copyright 2016 Maik Glatki
+#
+class oraclejava {
+
+
+}
